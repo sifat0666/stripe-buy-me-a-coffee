92a6136e8da44b962a5ab3ddc6f9ce346a084f0d
sifat <sifat0666@gmail.com>
2022-12-12 22:32:00 +0600

    add

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   const presets = [1, 3, 5];
   return (
     <div className="h-screen">
-      <h1>Buy me Beer</h1>
+      <h1>Buy me coffee</h1>
       {presets.map((preset) => (
         <button key={preset} onClick={() => setQuantity(preset)}>
           {preset}
@@ -23,7 +23,24 @@ export default function Home() {
         type="number"
         onChange={(e) => setQuantity(parseFloat(e.target.value))}
         max={MAX_DONATION_IN_CENTS / DONATION_IN_CENTS}
+        min={1}
+        defaultValue={1}
       />
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+            
+          onChange={(e) => setName(e.target.name)} /> 
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          value={message}
+          onChange={(e) => setMessage(message)}
+        />
+      </div>
       <button></button>
     </div>
   );
